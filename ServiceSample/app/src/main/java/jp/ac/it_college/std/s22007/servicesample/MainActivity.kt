a9b6faef8c0bf6f56ac6e9ba5b6e8787d25ab8a1
s22007 <s22007@it-college.ac.jp>
2023-10-12 14:27:37 +0900

    13章途中

--- ServiceSample/app/src/main/java/jp/ac/it_college/std/s22007/servicesample/MainActivity.kt
+++ ServiceSample/app/src/main/java/jp/ac/it_college/std/s22007/servicesample/MainActivity.kt
@@ -2,6 +2,7 @@ package jp.ac.it_college.std.s22007.servicesample
 
 import android.app.Activity
 import android.app.ActivityManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import jp.ac.it_college.std.s22007.servicesample.databinding.ActivityMainBinding
@@ -13,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btPlay.setOnClickListener{ startSoundManagerService() }
+        binding.btStop.setOnClickListener{ stopSoundManagerService() }
+    }
+
+    private fun startSoundManagerService(){
+        val intent = Intent(this, SoundManageService::class.java)
+        startService(intent)
+        binding.btPlay.isEnabled = false
+        binding.btStop.isEnabled = true
+    }
+    private fun stopSoundManagerService(){
+        val intent = Intent(this, SoundManageService::class.java)
+        stopService(intent)
+        binding.btPlay.isEnabled = false
+        binding.btStop.isEnabled = true
     }
 }
