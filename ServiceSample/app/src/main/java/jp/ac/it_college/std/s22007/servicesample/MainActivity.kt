9026533d062ac576d91d5a3349c33712fca126da
s22007 <s22007@it-college.ac.jp>
2023-10-18 11:52:18 +0900

    13.3終わり

--- ServiceSample/app/src/main/java/jp/ac/it_college/std/s22007/servicesample/MainActivity.kt
+++ ServiceSample/app/src/main/java/jp/ac/it_college/std/s22007/servicesample/MainActivity.kt
@@ -1,14 +1,28 @@
 package jp.ac.it_college.std.s22007.servicesample
 
-import android.app.Activity
-import android.app.ActivityManager
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import jp.ac.it_college.std.s22007.servicesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){isGranted ->
+            if (isGranted){
+                Log.i("SERVICE_SAMPLE", "通知の権限がもらえた")
+                startSoundManagerService()
+            }else{
+                Log.i("SERVICE_SAMPLE", "通知の権限がもらえなかった")
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +31,18 @@ class MainActivity : AppCompatActivity() {
 
         binding.btPlay.setOnClickListener{ startSoundManagerService() }
         binding.btStop.setOnClickListener{ stopSoundManagerService() }
+
+        val fromNotification = intent.getBooleanExtra("fromNotification", false)
+        if (fromNotification){
+            binding.btPlay.isEnabled = false
+            binding.btStop.isEnabled = true
+        }
     }
 
     private fun startSoundManagerService(){
+
+        if (!notificationPermissionRequest()) return
+
         val intent = Intent(this, SoundManageService::class.java)
         startService(intent)
         binding.btPlay.isEnabled = false
@@ -28,7 +51,32 @@ class MainActivity : AppCompatActivity() {
     private fun stopSoundManagerService(){
         val intent = Intent(this, SoundManageService::class.java)
         stopService(intent)
-        binding.btPlay.isEnabled = false
-        binding.btStop.isEnabled = true
+        binding.btPlay.isEnabled = true
+        binding.btStop.isEnabled = false
+    }
+
+    private fun notificationPermissionRequest(): Boolean{
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return true
+
+        return when{
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                true
+            }
+
+            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                false
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.POST_NOTIFICATIONS
+                )
+                false
+            }
+
+        }
     }
 }
--- ServiceSample/app/src/main/java/jp/ac/it_college/std/s22007/servicesample/SoundManageService.kt
+++ ServiceSample/app/src/main/java/jp/ac/it_college/std/s22007/servicesample/SoundManageService.kt
@@ -1,10 +1,21 @@
 package jp.ac.it_college.std.s22007.servicesample
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.IBinder
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+private const val CHANNEL_ID = "sound_manager_service_notification_channel"
 
 class SoundManageService : Service() {
     private var mediaPlayer: MediaPlayer? = null
@@ -15,19 +26,14 @@ class SoundManageService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-
         mediaPlayer = MediaPlayer()
-    }
 
-    override fun onDestroy() {
-        mediaPlayer?.run {
-            if (isPlaying){
-                stop()
-            }
-        }
-        super.onDestroy()
+        val name = getString(R.string.notification_channel_name)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance)
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
     }
-
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val mediaFileUri = Uri.parse(
             "android.resource://${packageName}/${R.raw.tanahara2}"
@@ -40,12 +46,48 @@ class SoundManageService : Service() {
         }
         return START_NOT_STICKY
     }
-
+    override fun onDestroy() {
+        mediaPlayer?.run {
+            if (isPlaying){
+                stop()
+            }
+        }
+        super.onDestroy()
+    }
+    @SuppressLint("MissingPermission")
     private fun onMediaPlayerPrepared(){
         mediaPlayer?.start()
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID).run {
+            setSmallIcon(android.R.drawable.ic_dialog_info)
+            setContentTitle(getString(R.string.msg_notification_text_start))
+            setContentText(getString(R.string.msg_notification_text_start))
+            val intent = Intent(this@SoundManageService, MainActivity::class.java).apply {
+                putExtra("formNotification", true)
+            }
+            val stopServiceIntent = PendingIntent.getActivity(
+                this@SoundManageService,
+                0,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+            setContentIntent(stopServiceIntent)
+            setAutoCancel(true)
+        }.build()
+        startForeground(200, notification)
     }
 
+    @SuppressLint("MissingPermission")
     private fun onPlaybackEnd(){
-        mediaPlayer?.stop()
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID).run {
+            setSmallIcon(android.R.drawable.ic_dialog_info)
+            setContentTitle(getString(R.string.msg_notification_text_finish))
+            setContentText(getString(R.string.msg_notification_text_finish))
+        }.build()
+        with(NotificationManagerCompat.from(this)) {
+            notify(100, notification)
+        }
+        stopSelf()
     }
+
+
 }
