212ff4b51cabe5117275c2d7c04ca218a2a04d3c
s22007 <s22007@it-college.ac.jp>
2023-10-25 10:42:08 +0900

    最初の画面表示

--- NavigationSample/app/src/main/java/jp/ac/it_college/std/s22007/navigationsample/MainActivity.kt
+++ NavigationSample/app/src/main/java/jp/ac/it_college/std/s22007/navigationsample/MainActivity.kt
@@ -1,11 +1,15 @@
 package jp.ac.it_college.std.s22007.navigationsample
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s22007.navigationsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ NavigationSample/app/src/main/java/jp/ac/it_college/std/s22007/navigationsample/fragment/HomeFragment.kt
@@ -0,0 +1,28 @@
+package jp.ac.it_college.std.s22007.navigationsample.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import jp.ac.it_college.std.s22007.navigationsample.R
+import jp.ac.it_college.std.s22007.navigationsample.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
